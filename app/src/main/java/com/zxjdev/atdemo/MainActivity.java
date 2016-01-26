6f7296e4f17a1d57881286b7629c42c9e10bb446
赵骁俊 <zxjzerg@163.com>
2016-01-26 10:37:23 +0800

    Create model user Update UI

--- app/src/main/java/com/zxjdev/atdemo/MainActivity.java
+++ app/src/main/java/com/zxjdev/atdemo/MainActivity.java
@@ -10,4 +10,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*public void updateOutput(Spannable ss) {
+        PlatoSpan[] spans1 = ss.getSpans(0, mEtSpannable.length(), PlatoSpan.class);
+
+        // 对span对象进行排序，在字符串靠前的排在前面
+        int position;
+        for (int i = 0; i < spans1.length; i++) {
+            int j = i + 1;
+            position = i;
+            PlatoSpan temp = spans1[i];
+            for (; j < spans1.length; j++) {
+                if (ss.getSpanStart(spans1[j]) < ss.getSpanStart(temp)) {
+                    temp = spans1[j];
+                    position = j;
+                }
+            }
+            spans1[position] = spans1[i];
+            spans1[i] = temp;
+        }
+
+        StringBuffer sb = new StringBuffer(ss);
+
+        for (int i = spans1.length; i > 0; i--) {
+            int spanStart = ss.getSpanStart(spans1[i - 1]);
+            int spanEnd = ss.getSpanEnd(spans1[i - 1]);
+            sb.replace(spanStart, spanEnd, "<" + spans1[i - 1].userid + ">");
+        }
+        mTvSpannable.setText(sb.toString());
+    }
+
+    public void insert(String text) {
+        int start = mEtSpannable.getSelectionStart();
+        mEtSpannable.getText().insert(start, text);
+        PlatoSpan span = new PlatoSpan() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "柏拉图 click");
+            }
+        };
+        span.userid = "789";
+        mEtSpannable.getText().setSpan(span, start, start + text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+    }*/
 }
---
+++ app/src/main/java/com/zxjdev/atdemo/User.java
@@ -0,0 +1,26 @@
+package com.zxjdev.atdemo;
+
+/**
+ * Created by andrew on 1/22/16.
+ */
+public class User {
+
+    private String userId;
+    private String nickname;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
